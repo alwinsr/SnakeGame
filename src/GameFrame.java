d1b1741d6649bff21e7d63dba362aaf786901d87
Alwin Sam Roy <alwinsamroy2002@gmail.com>
2024-07-16 00:48:41 +0530

    Added GameFrame class extending JFrame

---
+++ src/GameFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+class GameFrame extends JFrame {
+    void GrameFrame(){
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
